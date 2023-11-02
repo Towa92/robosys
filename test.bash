85ccba885e49a6af39047fe6d65494a0eb563148
Towa GD <towaakun@gmail.com>
2023-11-02 11:39:42 +0900

    add test line

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv 
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,5 +12,6 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 14 ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK
+[ "$res" = 0 ] &
+& echo OK
 exit $res
